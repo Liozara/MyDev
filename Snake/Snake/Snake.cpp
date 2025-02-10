b9709ef696d7ce2867cb3ca89b64f2300b861f58
Liozara <fbrcdanes@gmail.com>
2025-02-10 15:16:48 +0100

    []

--- Snake/Snake/Snake.cpp
+++ Snake/Snake/Snake.cpp
@@ -21,7 +21,7 @@ int main()
 	SetConsoleActiveScreenBuffer(hConsole);
 	DWORD dwBytesWritten = 0;
 
-	list <sSnakeSegment> snake = { {60,15}, {61,15} };
+	list <sSnakeSegment> snake = { {60,15}, {61,15}, {62,15}, {63,15} };
 	int nFoodX = 30;
 	int nFoodY = 15;
 	int nScore = 0;
@@ -39,6 +39,14 @@ int main()
 			screen[2 * nScreenWidth + i] = L'=';
 		}
 		wsprintf(&screen[nScreenWidth + 5], L"- - - SNAKE - - -              score: %d", nScore);
+
+		for (auto s : snake) {
+			screen[s.y * nScreenWidth + s.x] = bDead ? L'+' : L'o';
+		}
+
+		screen[snake.front().y * nScreenWidth + snake.front().x] = bDead ? L'X' : L'O';
+
+		screen[nFoodY * nScreenWidth + nFoodX] = L'$';
 
 		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
 	}
