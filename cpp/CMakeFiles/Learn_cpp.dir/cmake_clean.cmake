9221ef47b73347d9bb2af7ce70ea4a1d28c89a11
Liozara <fbrcdanes@gmail.com>
2025-02-11 17:07:30 +0100

    [MOD]

---
+++ cpp/CMakeFiles/Learn_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Learn_cpp.dir/link.d"
+  "CMakeFiles/Learn_cpp.dir/src/main.cpp.o"
+  "CMakeFiles/Learn_cpp.dir/src/main.cpp.o.d"
+  "Learn_cpp"
+  "Learn_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Learn_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
